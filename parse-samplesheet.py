8fc0a7b5fc0c88e38189f80eb3be23605d1f0581
cirrina <david.lindgren@med.lu.se>
2022-01-26 18:55:36 +0100

    biug fizes

--- parse-samplesheet.py
+++ parse-samplesheet.py
@@ -322,13 +322,21 @@ for project in all_projects:
                         if len(dfs[project]['Species'].unique()== 1):
                             current_row[1] = dfs[project]['Species'].tolist()[0]
                 if current_row[0]=='PipelineProfile':
-                    if 'PipelineProfile' in dfs[project].columns.tolist():
-                        if len(dfs[project]['PipelineProfile'].unique()== 1):
-                            current_row[1] = dfs[project]['PipelineProfile'].tolist()[0]
+                    if 'pipeline_profile' in dfs[project].columns.tolist():
+                        if len(dfs[project]['pipeline_profile'].unique()== 1):
+                            current_row[1] = dfs[project]['pipeline_profile'].tolist()[0]
                 if current_row[0]=='PoolName':
-                    if 'PoolName' in dfs[project].columns.tolist():
-                        if len(dfs[project]['PoolName'].unique()== 1):
-                            current_row[1] = dfs[project]['PoolName'].tolist()[0]
+                    if 'Sample_Pool' in dfs[project].columns.tolist():
+                        if len(dfs[project]['Sample_Pool'].unique()== 1):
+                            current_row[1] = dfs[project]['Sample_Pool'].tolist()[0]
+                if current_row[0]=='Assay':
+                    if 'Assay' in dfs[project].columns.tolist():
+                        if len(dfs[project]['Assay'].unique()== 1):
+                            current_row[1] = dfs[project]['Assay'].tolist()[0]
+                if current_row[0]=='Strandness':
+                    if 'Strandness' in dfs[project].columns.tolist():
+                        if len(dfs[project]['Strandness'].unique()== 1):
+                            current_row[1] = dfs[project]['Strandness'].tolist()[0]
                 ## Write row to file
                 if not all(elem == '' for elem in current_row):
                     writer.writerow(current_row)
